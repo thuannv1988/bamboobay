f8d2954d111cddc186df96a28e84f53817080a8d
Alex Nguyen <thuannv1988@gmail.com>
2025-07-20 23:45:57 -0400

    Add store route

--- app.py
+++ app.py
@@ -1,35 +1,37 @@
-from flask import Flask, render_template, request, redirect
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
-# Simulated database
-stores = {
-    "store1": {"name": "Store 1", "products": [{"id": 1, "name": "Shirt"}, {"id": 2, "name": "Shoes"}]},
-    "store2": {"name": "Store 2", "products": [{"id": 3, "name": "Hat"}]}
-}
-
-products = {
-    1: {"id": 1, "name": "Shirt", "desc": "Nice cotton shirt"},
-    2: {"id": 2, "name": "Shoes", "desc": "Running shoes"},
-    3: {"id": 3, "name": "Hat", "desc": "Cool summer hat"}
+store_data = {
+    "store1": [
+        {"id": 1, "name": "Product A", "price": "$10"},
+        {"id": 2, "name": "Product B", "price": "$15"},
+    ],
+    "store2": [
+        {"id": 3, "name": "Product C", "price": "$20"},
+        {"id": 4, "name": "Product D", "price": "$25"},
+    ],
 }
 
 @app.route('/')
 def home():
-    return render_template("home.html", stores=stores)
+    return render_template("home.html", stores=store_data.keys())
+
+@app.route('/store/<store_name>')
+def store_page(store_name):
+    if store_name in store_data:
+        products = store_data[store_name]
+        return render_template("store.html", store_name=store_name, products=products)
+    else:
+        return "Store not found", 404
 
 @app.route('/product/<int:product_id>')
-def product(product_id):
-    product = products.get(product_id)
-    return render_template("product.html", product=product)
+def product_page(product_id):
+    for products in store_data.values():
+        for product in products:
+            if product["id"] == product_id:
+                return render_template("product.html", product=product)
+    return "Product not found", 404
 
-@app.route('/', subdomain="<store>")
-def store_view(store):
-    store_data = stores.get(store)
-    if store_data:
-        return render_template("store.html", store=store_data)
-    return "Store not found", 404
-
-if __name__ == '__main__':
-    app.config['SERVER_NAME'] = "localhost:5000"  # for subdomain support
+if __name__ == "__main__":
     app.run(debug=True)
